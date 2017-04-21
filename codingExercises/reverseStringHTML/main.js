91bc183f52dd85702bf86b72d038ea1ff6fa2c53
Awful-Risky <iron.lake@gmail.com>
2017-04-21 16:03:39 -0600

    implemented reverse string on html with angular

--- codingExercises/reverseStringHTML/main.js
+++ codingExercises/reverseStringHTML/main.js
@@ -10,8 +10,21 @@ angular.module('angBasic', []); // Creating a module called angBasic
 angular.module('angBasic').controller('angTroller', [
 	'$scope', 
 	function($scope) {
-		console.log('Hello World');
-		console.log(angular);
-		$scope.testScope = 'Your On $Scope';
+		// console.log('Hello World');
+		// console.log(angular);
+		// $scope.testScope = 'Your On $Scope';
+
+	var reverseWord = function (word) {
+	// var splitWord = word.split("");
+	// console.log(splitWord);
+	// var reverseSplit = splitWord.reverse().join("");
+	// console.log(reverseSplit);
+	// console.log(word.split("").reverse().join(""));
+		return (word.split("").reverse().join(""));
+	};
+	$scope.reverse = function (string) {
+		$scope.what = reverseWord(string)
+	};
+
 
 }]);
