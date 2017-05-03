fb4bc83fe336d05a7daf541ed8768aa8ba974fb8
Awful-Risky <iron.lake@gmail.com>
2017-05-03 17:40:11 -0600

    more codeabbey exercises

---
+++ codingExercises/codeAbbey6.js
@@ -0,0 +1,29 @@
+// console.log(typeof(22/7))
+var num = (0.1)
+var negNum = (-22/7)
+// console.log('Original Number', num)
+// console.log(num % 1)
+
+function roundItUp (it) {
+	if (it > 0) {
+		if (it % 1 == 0.0) {
+			return it
+		}
+		else {
+			// console.log(it % 1)
+			let roundedIt = it;
+			let roundUpDifference = 1.0 - (it % 1)
+			// console.log(roundUpDifference)
+			roundedIt += roundUpDifference
+			return roundedIt
+		}
+	}
+	else {
+		// console.log('is it negative', it)
+		// console.log(it -= it % 1)
+		return (it -= it % 1);
+	}
+}
+
+console.log('Pos rounded up:',num,'=', roundItUp(num));
+console.log('Neg rounded up :',negNum,'=', roundItUp(negNum));
