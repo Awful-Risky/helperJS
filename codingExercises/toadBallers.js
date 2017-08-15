d0558874d2d133921df0528707e561908ad53357
Awful-Risky <iron.lake@gmail.com>
2017-08-14 21:36:46 -0600

    workin toadBallers

--- codingExercises/toadBallers.js
+++ codingExercises/toadBallers.js
@@ -1,11 +1,48 @@
+var underScore   = require('underscore');
+//npm install --save undersacore
+
+
+// var players = ['a',
+// 			   'b',
+// 			   'c',
+// 			   'd',
+// 			   'e',
+// 			   'f',
+// 			   'g',
+// 			   'h',
+// 			   'i',
+// 			   'j',
+// 			   'k',
+// 			   'l'];
+// var newplayers = [];
+
+
+// for (var i = 0; i < players.length; i++) {
+// 	// var num = i+1
+// 	var match = (players[i])
+// 	// console.log(num, match)
+// 	for (var j = 0; j < players.length; j++) {
+// 		if (players[j] !== players[i]) {
+// 			console.log({
+// 				p1 : players[j],
+// 				p2 : players[i],
+// 			})
+// 		}
+// 	}
+// 	console.log('BREAK')
 
 
 
-var array = [1,2,3,4,5,6,7,8,9,10,11,12]
+// } 
 
-for i in array
-var index = array.indexOf[i]
-var noIArray = array.splice(index, 1)
-for j in noIArray
+var in1 = [1,2];
+var in2 = [4,3];
+var test = in1.concat(in2);
+console.log(test);
+
+// var index = array.indexOf[i]
+// var noIinArray = array.splice(index, 1)
+// console.log(noIinArray);
+// for j in noIinArray
 
 
