c982b1617d37061f9bca993792ce64fecc198666
Awful-Risky <iron.lake@gmail.com>
2017-08-14 22:21:07 -0600

    working toadBallers js

--- codingExercises/toadBallers.js
+++ codingExercises/toadBallers.js
@@ -1,48 +1,42 @@
-var underScore   = require('underscore');
-//npm install --save undersacore
+var undScr   = require('underscore');
+//npm install --save undersacore 
+
+var players = ['a',
+			   'b',
+			   'c',
+			   'd',
+			   'e',
+			   'f',
+			   'g',
+			   'h',
+			   'i',
+			   'j',
+			   'k',
+			   'l'];
 
 
-// var players = ['a',
-// 			   'b',
-// 			   'c',
-// 			   'd',
-// 			   'e',
-// 			   'f',
-// 			   'g',
-// 			   'h',
-// 			   'i',
-// 			   'j',
-// 			   'k',
-// 			   'l'];
-// var newplayers = [];
+var newArray = [];
 
 
-// for (var i = 0; i < players.length; i++) {
-// 	// var num = i+1
-// 	var match = (players[i])
-// 	// console.log(num, match)
-// 	for (var j = 0; j < players.length; j++) {
-// 		if (players[j] !== players[i]) {
-// 			console.log({
-// 				p1 : players[j],
-// 				p2 : players[i],
-// 			})
-// 		}
-// 	}
-// 	console.log('BREAK')
+//Populate all possible combinations - *** Including duplicates
+function populateDirtFullSet (input) {
+	// dirtyFull list of all possible combos
+	for (var i = 0; i < input.length; i++) {
+		// console.log(i)
+		var match = (input[i])
+		// console.log(match)
+		for (var j = 0; j < input.length; j++) {
+			if (input[j] !== input[i]) {
+				var pusher = ({
+					p1 : input[j],
+					p2 : input[i],
+				})
+			}
+		}
+		newArray.push(pusher);
+		console.log(pusher); 
+		console.log('BREAK');
+	};
+};
 
-
-
-// } 
-
-var in1 = [1,2];
-var in2 = [4,3];
-var test = in1.concat(in2);
-console.log(test);
-
-// var index = array.indexOf[i]
-// var noIinArray = array.splice(index, 1)
-// console.log(noIinArray);
-// for j in noIinArray
-
-
+populateDirtFullSet(players);
